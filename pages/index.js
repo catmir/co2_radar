e6290ae40b101fc08f72b92f38234a38721e7dc0
catmir <mirsberger@catmir-marketing.de>
2023-11-14 12:12:26 +0100

    New index.js styling

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,9 @@
 import Link from 'next/link'
 import Head from 'next/head'
-import bg from '@/public/white-smoke-photo-marcin-jozwiak.jpg'
-import styles from "@/styles/Home.module.css"
 import Header from '@/components/Header'
 import Footer from '@/components/Footer'
 import dynamic from 'next/dynamic'
-import CustomTable from '@/components/CustomTable'
- 
+
 /* Disable Server Side Rendering for use of window object in Browser */
 const DynamicLanguage = dynamic(() => import('@/components/Language'), {
   ssr: false,
@@ -26,49 +23,76 @@ export default function Home() {
 
       <Header/>
 
-      <main>
-        <div className="text-center bg-image">
-          <div className="mask"
-            style={{
-              backgroundImage: `url(${bg.src})`,
-              height: '100vh',
-              width: '100vw',
-              backgroundPosition: 'center',
-              backgroundRepeat: 'no-repeat',
-              backgroundSize: 'cover',
-              position: 'relative'
-            }}
-          >
-            <div className="d-flex justify-content-center align-items-center h-100">
-              <div className="text-white">
-                <h1 className="mb-3">CO<sub>2</sub> Radar</h1>
-                <h2 className="mb-3">Observing the World's Polluters</h2>
-                <Link className="btn btn-outline-light btn-lg" href="#home" role="button">Latest report</Link>
+      <main className="page-wrapper">
+
+        <section className="bg-dark dark-mode position-relative pt-2 pb-5">
+          <div className="container position-relative zindex-2 py-md-3 py-lg-4 py-xl-5">
+            <div className="row align-items-center pt-5 pb-2 pb-sm-3 mt-sm-4">
+              <div className="col-6 text-light">
+                <h1 className="display-3 text-uppercase fw-semibold pb-3 mb-4">Monitoring the World's Polluters</h1>
+                <p className="fs-5 fw-light pb-2 pb-md-4 pb-xl-5 mb-xl-2">Take a look at the carbon dioxide emissons data that we are continuously collecting from companies around the globe.</p>
+                <div className="d-sm-flex">
+                  <Link className="btn btn-outline-light fs-5 px-4 py-2 mb-sm-0 me-sm-3 me-lg-4" href="#latest-reports" role="button">Latest CO<sub>2</sub> Reports</Link>
+                </div>
+              </div>
+              <div className="col-6 h-100 overflow-hidden d-none d-lg-block">
+                <img src="/white-smoke-photo-marcin-jozwiak.jpg" width='100%' height='100%' className="rounded-circle" alt="Picture of carbon dioxide emitting factory"/>
               </div>
             </div>
           </div>
-        </div>
+        </section>
 
-        <div className="container my-5">
-          <div className="row">
-            
-            <DynamicLanguage/>
-            
-            <div className="row my-5">
-              <h2 id="home">Report 2022</h2>
-              
-              <p className="fs-5">
-              Carbondioxide is one of the most dangerous greenhouse gas emissions in the world.</p>
-              <p className={styles.highlight}>Watch out for the worst polluters:</p>
-
-            </div>
-              <CustomTable/>
-            <div>
-             
-            
+        <section className="bg-secondary position-relative py-4 py-md-5">
+          <div className="container position-relative zindex-2">
+            <div className="row">
+              <div className="col-3 d-flex justify-content-md-center">
+                <div className="py-3 my-lg-2 my-xl-3">
+                  <div className="text-dark fw-semibold display-2 text-center mb-1">41</div>
+                  <p className="fs-xl text-center mb-0">Companies</p>
+                </div>
+              </div>
+              <div className="col-3 d-flex justify-content-md-center">
+                <div className="py-3 ps-xl-4 my-lg-2 my-xl-3">
+                  <div className="text-dark fw-semibold display-2 text-center mb-1">15</div>
+                  <p className="fs-xl text-center mb-0">Countries</p>
+                </div>
+              </div>
+              <div className="col-3 d-flex justify-content-md-center">
+                <div className="py-3 my-lg-2 my-xl-3">
+                  <div className="text-dark fw-semibold display-2 text-center mb-1">3.3</div>
+                  <p className="fs-xl text-center mb-0">Billion Tons of CO<sub>2</sub></p>
+                </div>
+              </div>
+              <div className="col-3 d-flex justify-content-md-center">
+                <div className="py-3 pe-xl-4 my-lg-2 my-xl-3">
+                  <div className="text-dark fw-semibold display-2 text-center mb-1">24/7</div>
+                  <p className="fs-xl text-center mb-0">Under Surveillance</p>
+                </div>
+              </div>
             </div>
           </div>
-        </div>
+        </section>
+
+        <section id="latest-reports" className="container py-5 my-xl-3 my-xxl-5">
+          <div className="row pt-1 pt-sm-3 pt-md-4 pt-lg-5 mt-xl-2">
+            <div className="col-4">
+              <h2 className="pb-1 pb-sm-2 mb-4">Our latest reports</h2>
+            </div>
+            <div className="col-8">
+              <DynamicLanguage/>
+              <p className="fs-5 fw-light pt-4">We have been monitoring and documenting the CO<sub>2</sub> emissions of companies from all kinds of industries for many years.</p>
+              <p className="fs-5 fw-light">Our wealth of data is a valuable source for politicians, journalists and environmental activists worldwide to better understand the extent of past and present greenhouse gas emissions.</p>
+            </div>
+          </div>
+        </section>
+
+        <section className="card bg-primary mt-6 mx-5 py-3">
+          <div className="card-body position-relative d-sm-flex align-items-center justify-content-evenly zindex-2 text-center">
+            <h2 className="card-text text-light">Do you want to support us?</h2>
+            <Link className="btn btn-light fs-5 fw-medium py-3 px-5" href="/contribute">Click here!</Link>
+          </div>
+        </section>
+
       </main>
 
       <Footer/>
