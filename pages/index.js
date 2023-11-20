feed3299f62c4df1970c3b4b882fd74d4245af4d
catmir <mirsberger@catmir-marketing.de>
2023-11-20 18:15:50 +0100

    Responsive Layout Optimization

--- pages/index.js
+++ pages/index.js
@@ -26,16 +26,16 @@ export default function Home() {
       <main className="page-wrapper">
 
         <section className="bg-dark dark-mode position-relative pt-2 pb-5">
-          <div className="container position-relative zindex-2 py-md-3 py-lg-4">
-            <div className="row align-items-center pt-5 pb-2 pb-sm-3 mt-sm-4">
-              <div className="col-6 text-light">
-                <h1 className="display-3 text-uppercase fw-semibold pb-3 mb-4">Monitoring the World's Polluters</h1>
-                <p className="fs-5 fw-light pb-2 pb-md-4 pb-xl-5 mb-xl-2">Take a look at the carbon dioxide emissons data that we are continuously collecting from companies around the globe.</p>
-                <div className="d-sm-flex">
+          <div className="container position-relative zindex-2 py-5 py-md-4">
+            <div className="row row-cols-lg-2 align-items-center pt-5 pb-2 pb-sm-3 mt-sm-4">
+              <div className="col text-light">
+                <h1 className="display-3 text-uppercase fw-semibold text-center text-md-start pb-3 mb-4">Monitoring the World's Polluters</h1>
+                <p className="fs-5 fw-light text-center text-md-start pb-4 pb-xl-5 mb-xl-2">Take a look at the carbon dioxide emissons data that we are continuously collecting from companies around the globe.</p>
+                <div className="text-center text-md-start">
                   <Link className="btn btn-outline-light fs-5 px-4 py-2 mb-sm-0 me-sm-3 me-lg-4" href="#latest-reports" role="button">Latest CO<sub>2</sub> Reports</Link>
                 </div>
               </div>
-              <div className="col-6 h-100 overflow-hidden d-none d-lg-block">
+              <div className="col h-100 overflow-hidden d-none d-lg-block">
                 <img src="/white-smoke-photo-marcin-jozwiak.jpg" width='100%' height='100%' className="rounded-circle" alt="Picture of carbon dioxide emitting factory"/>
               </div>
             </div>
@@ -44,26 +44,26 @@ export default function Home() {
 
         <section className="bg-secondary position-relative py-4 py-md-5">
           <div className="container position-relative zindex-2">
-            <div className="row">
-              <div className="col-3 d-flex justify-content-md-center">
+            <div className="row row-cols-2 row-cols-md-4">
+              <div className="col d-flex justify-content-center">
                 <div className="py-3 my-lg-2 my-xl-3">
                   <div className="text-dark fw-semibold display-2 text-center mb-1">41</div>
                   <p className="fs-xl text-center mb-0">Companies</p>
                 </div>
               </div>
-              <div className="col-3 d-flex justify-content-md-center">
+              <div className="col d-flex justify-content-center">
                 <div className="py-3 ps-xl-4 my-lg-2 my-xl-3">
                   <div className="text-dark fw-semibold display-2 text-center mb-1">15</div>
                   <p className="fs-xl text-center mb-0">Countries</p>
                 </div>
               </div>
-              <div className="col-3 d-flex justify-content-md-center">
+              <div className="col d-flex justify-content-center">
                 <div className="py-3 my-lg-2 my-xl-3">
                   <div className="text-dark fw-semibold display-2 text-center mb-1">3.3</div>
                   <p className="fs-xl text-center mb-0">Billion Tons of CO<sub>2</sub></p>
                 </div>
               </div>
-              <div className="col-3 d-flex justify-content-md-center">
+              <div className="col d-flex justify-content-center">
                 <div className="py-3 pe-xl-4 my-lg-2 my-xl-3">
                   <div className="text-dark fw-semibold display-2 text-center mb-1">24/7</div>
                   <p className="fs-xl text-center mb-0">Under Surveillance</p>
@@ -75,10 +75,10 @@ export default function Home() {
 
         <section id="latest-reports" className="container py-5 my-xl-3 my-xxl-5">
           <div className="row pt-1 pt-sm-3 pt-md-4 pt-lg-5 mt-xl-2">
-            <div className="col-4">
+            <div className="col-xl-4">
               <h2 className="pb-1 pb-sm-2 mb-4">Our latest reports</h2>
             </div>
-            <div className="col-8">
+            <div className="col-xl-8">
               <DynamicLanguage/>
               <p className="fs-5 fw-light pt-4">We have been monitoring and documenting the CO<sub>2</sub> emissions of companies from all kinds of industries for many years.</p>
               <p className="fs-5 fw-light">Our wealth of data is a valuable source for politicians, journalists and environmental activists worldwide to better understand the extent of past and present greenhouse gas emissions.</p>
