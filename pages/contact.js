aeca43f3ed95ebc9588e122a2b73458c7747a440
catmir <mirsberger@catmir-marketing.de>
2023-11-21 14:24:03 +0100

    All Pages Responsiveness

--- pages/contact.js
+++ pages/contact.js
@@ -1,6 +1,7 @@
 import Head from "next/head";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
+import Link from "next/link";
 
 export default function Contact() {
     const title = 'CO\u2082 Radar – Contact Us';
@@ -16,11 +17,22 @@ export default function Contact() {
     
           <Header/>
     
-          <main>
-          <div className="container my-5">
-            <h1>Contact Us</h1>
-            <p className="fs-5">Descriptive Text</p>
-          </div>
+          <main className="page-wrapper">
+
+            <section className="position-relative py-5">
+              <div className="container position-relative zindex-2 py-md-3 py-lg-5">
+                <div className="row align-items-start pt-5 pb-2 pb-sm-3 mt-sm-4">
+                  <h1 className="fs-1 fw-semibold lh-base">Please Contact Us</h1>
+                  <div className="row align-items-start mt-5">
+                    <h2 className="fs-4 mb-5">CCIR Climate Change Is Real, Inc</h2>
+                    <p className="fs-5 fw-light">Green Building 4U, 12345 City of Ideals</p>
+                    <p className="fs-5 fw-light">+123 xxx xxxx xxx xxx</p>
+                    <p className="fs-5 fw-light">E-Mail: <Link className="text-primary" href="#">contact@ccir.com</Link></p>
+                  </div>
+                </div>
+              </div>
+            </section>
+
           </main>
     
           <Footer/>
--- pages/terms-and-conditions.js
+++ pages/terms-and-conditions.js
@@ -16,11 +16,19 @@ export default function TermsAndConditions() {
     
           <Header/>
     
-          <main>
-          <div className="container my-5">
-            <h1>Terms & Conditions</h1>
-            <p className="fs-5">Descriptive Text</p>
-          </div>
+          <main className="page-wrapper">
+
+            <section className="position-relative py-5">
+              <div className="container position-relative zindex-2 py-md-3 py-lg-5">
+                <div className="row align-items-start pt-5 pb-2 pb-sm-3 mt-sm-4">
+                  <h1 className="fs-1 fw-semibold lh-base">Our Terms & Conditions</h1>
+                  <div className="row align-items-start pt-3 pb-2 pb-sm-3 mt-sm-4">
+                    <p className="col fs-5 fw-light">Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim.</p>
+                  </div>
+                </div>
+              </div>
+            </section>
+
           </main>
     
           <Footer/>
